89cd7def6ec48a4066e1ca514da1b328867001da
Olutoyosi Kuti <okuti2@myseneca.ca>
2023-01-29 15:43:08 -0500

    Updating

--- assets/js/main.js
+++ assets/js/main.js
@@ -231,21 +231,28 @@
    */
   new PureCounter();
 
-  document.getElementById("email-form").addEventListener("submit", function(event){
-    event.preventDefault();
+statusTxt = document.getElementById("#button-area");
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://formsubmit.co/93c2be4ac22ebb19d589219367bd4b29", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            // Handle the response here
-            console.log(xhr.responseText);
-        }
-    };
-    var formData = new FormData(document.getElementById("email-form"));
-    xhr.send(formData);
+document.getElementById("email-form").addEventListener("submit", function(event){
+  event.preventDefault();
+  statusTxt.style.color = "#0D6EFD";
+  statusTxt.style.display = "block";
+  statusTxt.innerText = "Sending your message...";
+  form.classList.add("disabled");
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://formsubmit.co/93c2be4ac22ebb19d589219367bd4b29", true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+          // Handle the response here
+          console.log(xhr.responseText);
+      }
+  };
+  var formData = new FormData(document.getElementById("email-form"));
+  xhr.send(formData);
 });
+
 
 
 })()
