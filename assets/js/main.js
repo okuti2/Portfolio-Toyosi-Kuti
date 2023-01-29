713aadbbdabeee6190ad3934ad521ebe5b36e80b
Olutoyosi Kuti <okuti2@myseneca.ca>
2023-01-29 15:14:05 -0500

    Updating files

--- assets/js/main.js
+++ assets/js/main.js
@@ -231,36 +231,21 @@
    */
   new PureCounter();
 
-  //Contact Form in PHP
-const form = document.querySelector("form"),
-statusTxt = form.querySelector("#button-area");
-form.onsubmit = (e)=>{
-  e.preventDefault();
-  statusTxt.style.color = "#0D6EFD";
-  statusTxt.style.display = "block";
-  statusTxt.innerText = "Sending your message...";
-  form.classList.add("disabled");
+  document.getElementById("email-form").addEventListener("submit", function(event){
+    event.preventDefault();
 
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "message.php", true);
-  xhr.onload = ()=>{
-    if(xhr.readyState == 4 && xhr.status == 200){
-      let response = xhr.response;
-      if(response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1){
-        statusTxt.style.color = "red";
-      }else{
-        form.reset();
-        setTimeout(()=>{
-          statusTxt.style.display = "none";
-        }, 3000);
-      }
-      statusTxt.innerText = response;
-      form.classList.remove("disabled");
-    }
-  }
-  let formData = new FormData(form);
-  xhr.send(formData);
-}
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://formsubmit.co/93c2be4ac22ebb19d589219367bd4b29", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Handle the response here
+            console.log(xhr.responseText);
+        }
+    };
+    var formData = new FormData(document.getElementById("email-form"));
+    xhr.send(formData);
+});
 
 
 })()
